8e18e433230e26aad6b154740bf4df388d8fbe6d
Андрей Прокопец <126653033+aprokopetsphilips@users.noreply.github.com>
2023-08-29 12:18:32 +0300

    Update lets_scrap.py
    добавил комментарии

--- lets_scrap.py
+++ lets_scrap.py
@@ -6,8 +6,9 @@ from time import sleep
 import json
 import fake_useragent
 
-headers = {'User-Agent':'Mozilla/5.0 (iPod touch; CPU iPhone 16_4_1 like Mac OS X) AppleWebKit/605.1.15 (HTML, like Gecko) Version/16.4 Mobile/15E148 Safari/604.1'}
+headers = {'User-Agent': 'Mozilla/5.0 (iPod touch; CPU iPhone 16_4_1 like Mac OS X) AppleWebKit/605.1.15 (HTML, like Gecko) Version/16.4 Mobile/15E148 Safari/604.1'}
 
+# Функция для получения ссылок на вакансии
 def get_links(text):
     user_agent = fake_useragent.UserAgent()
     data = requests.get(url=f'https://hh.ru/search/vacancy?text={text}&area=113&page=1',
@@ -31,57 +32,43 @@ def get_links(text):
         except Exception as e:
             print(f'{e}')
 
-
-
+# Функция для получения данных о вакансии
 def get_resume(link):
     user_agent = fake_useragent.UserAgent()
-    data = requests.get(url=link,headers=headers)
+    data = requests.get(url=link, headers=headers)
     if data.status_code != 200:
         return
     soup = BeautifulSoup(data.content, 'lxml')
     try:
-        name = soup.find(attrs={'class':'vacancy-title'}).text
+        name = soup.find(attrs={'class': 'vacancy-title'}).text
     except:
-        name =''
+        name = ''
     try:
-        salary = soup.find(attrs={'class':'bloko-header-section-2 bloko-header-section-2_lite'}).text.replace('\\xa','')
+        salary = soup.find(attrs={'class': 'bloko-header-section-2 bloko-header-section-2_lite'}).text.replace('\\xa','')
     except:
-        salary =''
+        salary = ''
     try:
         vacancy_description = soup.find(attrs={'class': 'g-user-content'}).text
     except:
         vacancy_description = ''
     try:
-        skills = [skill.text for skill in soup.find(attrs={'class':'bloko-tag-list'}).find_all(attrs={'class': 'bloko-tag__section_text'})]
+        skills = [skill.text for skill in soup.find(attrs={'class': 'bloko-tag-list'}).find_all(attrs={'class': 'bloko-tag__section_text'})]
     except:
-        skills =[]
+        skills = []
     resume = {'name': name,
               'salary': salary,
-              'vacancy_description':vacancy_description,
+              'vacancy_description': vacancy_description,
               'skills': skills,
-              'adr':link
+              'adr': link
               }
 
-    return  resume
-
-
-
-
-"""data = []
-for a in get_links('python'):
-    writer(get_resume(a))
-    time.sleep(1)
-
-
-if __name__ == '__main__':
-    for a in get_links('python'):
-        print(get_resume(a))
-        time.sleep(1)"""
+    return resume
 
 if __name__ == '__main__':
     data = []
     for a in get_links('python'):
         data.append(get_resume(a))
         time.sleep(1)
-        with open('data.json','w',encoding='utf-8') as f:
-            json.dump(data,f,indent=4,ensure_ascii=False)
+        # Сохраняем данные в JSON-файл
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
